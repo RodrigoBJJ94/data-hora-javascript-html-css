61d15e0692a587039055e157687cbf781998dff3
Rodrigo Brentano <rodrigobjj24@gmail.com>
2021-07-24 22:34:58 -0300

    Version 1.92

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,13 +5,16 @@ function getDiaSemana(diaSemana) {
     const dia = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
     return dia[diaSemana];
 }
+
 function getMes(mes) {
     const meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
     return meses[mes]
 }
+
 function zeroAEsquerda(numero) {
     return numero >= 10 ? numero : `0${numero}`;
 }
+
 function criarData(data) {
     const diaSemana = data.getDay();
     const mes = data.getMonth();
@@ -22,4 +25,5 @@ function criarData(data) {
         `${nomeDiaSemana}, ${data.getDate()} de ${nomeMes} de ${data.getFullYear()}, ${zeroAEsquerda(data.getHours())}:${zeroAEsquerda(data.getMinutes())}:${zeroAEsquerda(data.getSeconds())}`
         );
 }
+
     h1.innerHTML = criarData(data);
